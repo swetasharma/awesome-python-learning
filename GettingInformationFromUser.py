d39e8a227380ecf4af059b3d630c190904f638e7
Sweta Sharma <sweta.sharma@siemens.com>
2020-05-08 21:02:51 +0530

    formatting strings in python

--- GettingInformationFromUser.py
+++ GettingInformationFromUser.py
@@ -1,3 +1,4 @@
+#input - we ask input from the user
 name = input('enter your name: ')
 print(name)
 
@@ -5,5 +6,21 @@ print(name)
 print('Adding Numbers')
 x = 42 + 206
 print('Performing division')
-y = x / 0
+# y = x / 0
 print('Math Complete')
+
+#Commenting lines of code can help you debug and figure out which line of code is causing an error
+# Make habbits of using Commenting both for debugging and Commenting
+
+first_name = 'Sweta'
+print(first_name)
+# you can combine strings using +
+
+# you can use functions to modify strings
+sentence = 'The world is beautiful'
+print(sentence.upper())
+
+# Custom string formatting
+last_name = 'Sharma'
+output = f'Hello, {first_name} {last_name}'
+print(output)
