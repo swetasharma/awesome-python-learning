844f300e818cd96878c3de234d516a678a6884f2
Sweta Sharma <sweta.sharma@siemens.com>
2020-05-08 17:44:37 +0530

    print is useful tool for debugging code

---
+++ GettingInformationFromUser.py
@@ -0,0 +1,9 @@
+name = input('enter your name: ')
+print(name)
+
+#Debugging with print / print is useful tool for debugging code
+print('Adding Numbers')
+x = 42 + 206
+print('Performing division')
+y = x / 0
+print('Math Complete')
